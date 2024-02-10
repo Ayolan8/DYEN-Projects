3d9241ae0af9659fb53c17869011d44befeaea13
Ijalana Afolabi <iijalana@gmail.com>
2024-02-10 17:57:40 +0100

    update on the flexbox

--- flexbox/script.js
+++ flexbox/script.js
@@ -6,11 +6,40 @@ const displaySideBar = function() {
     displayBtn.classList.toggle('hidden');
 }*/
 
-const accordion = document.getElementsByClassName('accordion');
+// const accordion = document.getElementsByClassName('accordion');
 const openAccordion = document.getElementsByClassName('open-accordion');
 const closeAccordion = document.getElementsByClassName('close-accordion');
+const panel = document.getElementsByClassName('panel');
 
-for (let i = 0; i < accordion.length; i++){
+const openAcc = () => {
+    for (let i = 0; i < openAccordion.length; i++){
+        openAccordion[i].addEventListener("click", function(){
+            this.classList.add('hidden');
+
+            closeAccordion[i].classList.remove('hidden');
+            panel[i].style.display = "none";
+        }); 
+    }
+    for (let j = 0; j < closeAccordion.length; j++){
+        closeAccordion[j].addEventListener("click", function() {
+            this.classList.add('hidden');
+            
+            openAccordion[j].classList.remove('hidden');
+            panel[j].style.display = "block";
+        })   
+    }
+}
+    
+openAcc();
+
+// get the sidebar menu when media screen is at max-width of 680px
+const sidebarMenu = document.getElementById('display-sidebar-menu');
+
+sidebarMenu.addEventListener("click", function(){
+    sidebarMenu.classList.toggle("active");
+})
+
+/*for (let i = 0; i < accordion.length; i++){
     accordion[i].addEventListener("click", function() {
         let j, k;
         for (j = 0; j < openAccordion.length; j++) {
@@ -18,4 +47,13 @@ for (let i = 0; i < accordion.length; i++){
         }
         this.classList.remove('hidden');
     })
-}
+    for (let x = 0; x < panel.length; x++){
+        if (panel[x].style.display == "block"){
+            panel[x].style.display = "none"
+        }else {
+            panel[x].style.display = "block";
+        }
+    }
+    
+}*/
+
